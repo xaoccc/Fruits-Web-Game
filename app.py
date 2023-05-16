c9f380ee7c441437525adad7930b6e9933258f15
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-16 23:50:47 +0300

    asd

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def fire_top():
     return fire(position, 0, 1)
 
 
-@app.route('FireBottom')
+@app.route('/FireBottom')
 def fire_bottom():
     position = int(request.args['position'])
     return fire(position, rows - 1, -1)
